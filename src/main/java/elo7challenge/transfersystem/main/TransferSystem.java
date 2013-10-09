c72d02089bb2c75744824981ea7e0d05e18e57c0
Jeff Otuka <jeffotuka@gmail.com>
2013-10-09 12:07:44 -0300

    Placeholder classes for main and service

---
+++ src/main/java/elo7challenge/transfersystem/main/TransferSystem.java
@@ -0,0 +1,8 @@
+package elo7challenge.transfersystem.main;
+
+public class TransferSystem {
+
+	public static void main(String[] args) {
+		System.out.println("hi, it worked.");
+	}
+}
---
+++ src/main/java/elo7challenge/transfersystem/service/TransferManager.java
@@ -0,0 +1,13 @@
+package elo7challenge.transfersystem.service;
+
+import java.util.List;
+
+import elo7challenge.transfersystem.domain.FinancialTransfer;
+
+public class TransferManager {
+
+	public List<FinancialTransfer> listAllTransfers() {
+		throw new UnsupportedOperationException("implement soon");
+	}
+	
+}
