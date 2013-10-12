b42a9a933ec4e77b238f86d41e171a35833f3c0b
Jeff Otuka <jeffotuka@gmail.com>
2013-10-12 18:32:38 -0300

    First runnable version, with setup and list

--- src/main/java/elo7challenge/transfersystem/main/TransferSystem.java
+++ src/main/java/elo7challenge/transfersystem/main/TransferSystem.java
@@ -1,8 +1,34 @@
 package elo7challenge.transfersystem.main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import elo7challenge.transfersystem.console.ArgsCommand;
+import elo7challenge.transfersystem.console.ArgsInterpreter;
+import elo7challenge.transfersystem.console.CommandRunner;
+import elo7challenge.transfersystem.console.CommandRunnerFactory;
+
 public class TransferSystem {
 
-	public static void main(String[] args) {
-		System.out.println("hi, it worked.");
+	public static void main(String[] args) throws SQLException, ParseException {
+		Connection connection = null;
+		try {
+			ArgsInterpreter argsInterpreter = new ArgsInterpreter();
+			ArgsCommand command = argsInterpreter.identifyCommand(args);
+
+			connection = DriverManager.getConnection("jdbc:hsqldb:file:db/dbtransfer", "SA", "");
+			CommandRunner commandRunner = new CommandRunnerFactory().build(connection, System.out);
+			commandRunner.run(command);
+			
+		} catch (SQLException e) {
+			throw e;
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
 	}
 }
