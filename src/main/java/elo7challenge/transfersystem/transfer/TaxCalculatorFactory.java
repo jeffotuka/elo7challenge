89986fea1b0242c61dcfcbe921b6f3ac31f70a14
Jeff Otuka <jeffotuka@gmail.com>
2013-10-13 22:24:16 -0300

    Tax calculator factory support for Type A to D transfers

--- src/main/java/elo7challenge/transfersystem/transfer/TaxCalculatorFactory.java
+++ src/main/java/elo7challenge/transfersystem/transfer/TaxCalculatorFactory.java
@@ -1,12 +1,44 @@
 package elo7challenge.transfersystem.transfer;
 
+import java.math.BigDecimal;
+
 import elo7challenge.transfersystem.domain.FinancialTransferType;
+import elo7challenge.transfersystem.time.Clock;
 import elo7challenge.transfersystem.transfer.calculation.TypeATaxCalculator;
+import elo7challenge.transfersystem.transfer.calculation.TypeBTaxCalculator;
+import elo7challenge.transfersystem.transfer.calculation.TypeCTaxCalculator;
 
 public class TaxCalculatorFactory {
 
-	public TaxCalculator build(FinancialTransferType type) {
-		return new TypeATaxCalculator();
+	public TaxCalculator build(FinancialTransferType type, BigDecimal value) {
+		if (type == null) {
+			throw new IllegalArgumentException("type is null");
+		} else if (FinancialTransferType.D.equals(type) && value == null) {
+			throw new IllegalArgumentException("value is null");
+		}
+		
+		TaxCalculator calculator = null;
+
+		FinancialTransferType derivedType = type;
+		if (FinancialTransferType.D.equals(type)) {
+			if (value.compareTo(new BigDecimal("25001.00")) < 0) {
+				derivedType = FinancialTransferType.A;
+			} else if (value.compareTo(new BigDecimal("120001.00")) < 0) {
+				derivedType = FinancialTransferType.B;
+			} else {
+				derivedType = FinancialTransferType.C;
+			}
+		}
+		
+		if (FinancialTransferType.A.equals(derivedType)) {
+			calculator = new TypeATaxCalculator();
+		} else if (FinancialTransferType.B.equals(derivedType)) {
+			calculator = new TypeBTaxCalculator(new Clock());
+		} else if (FinancialTransferType.C.equals(derivedType)) {
+			calculator = new TypeCTaxCalculator(new Clock());
+		} 
+		
+		return calculator;
 	}
 	
 }
--- src/test/java/elo7challenge/transfersystem/transfer/TaxCalculatorFactoryTest.java
+++ src/test/java/elo7challenge/transfersystem/transfer/TaxCalculatorFactoryTest.java
@@ -2,11 +2,15 @@ package elo7challenge.transfersystem.transfer;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import elo7challenge.transfersystem.domain.FinancialTransferType;
 import elo7challenge.transfersystem.transfer.calculation.TypeATaxCalculator;
+import elo7challenge.transfersystem.transfer.calculation.TypeBTaxCalculator;
+import elo7challenge.transfersystem.transfer.calculation.TypeCTaxCalculator;
 
 public class TaxCalculatorFactoryTest {
 
@@ -19,9 +23,61 @@ public class TaxCalculatorFactoryTest {
 	
 	@Test
 	public void shouldBuildTypeATaxCalculator() {
-		TaxCalculator calculator = factory.build(FinancialTransferType.A);
+		TaxCalculator calculator = factory.build(FinancialTransferType.A, null);
 		
 		assertEquals(TypeATaxCalculator.class, calculator.getClass());
+	}
+
+	@Test
+	public void shouldBuildTypeBTaxCalculator() {
+		TaxCalculator calculator = factory.build(FinancialTransferType.B, null);
+		
+		assertEquals(TypeBTaxCalculator.class, calculator.getClass());
+	}
+
+	@Test
+	public void shouldBuildTypeCTaxCalculator() {
+		TaxCalculator calculator = factory.build(FinancialTransferType.C, null);
+		
+		assertEquals(TypeCTaxCalculator.class, calculator.getClass());
+	}
+
+	@Test
+	public void shouldBuildTypeATaxCalculatorForTypeDWithLowValue() {
+		TaxCalculator calculator = factory.build(FinancialTransferType.D, new BigDecimal("25000.99"));
+		
+		assertEquals(TypeATaxCalculator.class, calculator.getClass());
+	}
+
+	@Test
+	public void shouldBuildTypeBTaxCalculatorForTypeDWithMiddleStartValue() {
+		TaxCalculator calculator = factory.build(FinancialTransferType.D, new BigDecimal("25001.00"));
+		
+		assertEquals(TypeBTaxCalculator.class, calculator.getClass());
+	}
+
+	@Test
+	public void shouldBuildTypeBTaxCalculatorForTypeDWithMiddleEndValue() {
+		TaxCalculator calculator = factory.build(FinancialTransferType.D, new BigDecimal("120000.99"));
+		
+		assertEquals(TypeBTaxCalculator.class, calculator.getClass());
+	}
+
+	@Test
+	public void shouldBuildTypeCTaxCalculatorForTypeDWithHighStartValue() {
+		TaxCalculator calculator = factory.build(FinancialTransferType.D, new BigDecimal("120001.00"));
+		
+		assertEquals(TypeCTaxCalculator.class, calculator.getClass());
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void shouldThrowExceptionWhenTypeIsDAndValueIsNull() {
+		factory.build(FinancialTransferType.D, null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void shouldThrowExceptionWhenTypeIsNull() {
+		factory.build(null, null);
 	}
 
 }
